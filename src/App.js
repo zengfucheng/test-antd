fc847742eca564d2957533339340eba667fc65a0
zengfucheng <178378772@qq.com>
2018-12-28 11:21:45 +0800

    1

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React10
+            Learn React100
           </a>
         </header>
       </div>
